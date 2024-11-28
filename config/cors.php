3834cc75d1142a3900fe0213109281df5e7e1401
Gauro11 <milbertgaringa5@gmail.com>
2024-11-28 10:30:05 +0800

    correct cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://pagsasaka.bpc-bsis4d.com/'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
